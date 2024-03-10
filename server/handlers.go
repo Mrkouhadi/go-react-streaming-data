cfd8f3badd71b1201a7b292c0bd3f4273b42d659
mrkouhadi <teacherbryan@outlook.com>
2024-03-10 23:47:56 +0800

    feat: added new handler of streaming data using channels

--- server/handlers.go
+++ server/handlers.go
@@ -18,3 +18,38 @@ func handleEventsStream(w http.ResponseWriter, r *http.Request) {
 		time.Sleep(1 * time.Second) // Simulate periodic updates
 	}
 }
+
+
+// new handler to stream data using channels
+func eventsHandler(w http.ResponseWriter, r *http.Request) {
+	// Set headers for Server-Sent Events
+	w.Header().Set("Content-Type", "text/event-stream")
+	w.Header().Set("Cache-Control", "no-cache")
+	w.Header().Set("Connection", "keep-alive")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+
+	// Create a channel to send data to the client
+	dataChannel := make(chan string)
+
+	// Close the channel when the request is closed
+	defer close(dataChannel)
+
+	// Simulate continuous data generation
+	go func() {
+		for i := 0; i < 10; i++ {
+			time.Sleep(1 * time.Second)
+			dataChannel <- fmt.Sprintf("Message %d", i)
+		}
+	}()
+
+	// Continuously send data to the client
+	for {
+		select {
+		case data := <-dataChannel:
+			fmt.Fprintf(w, "data: %s\n\n", data)
+			w.(http.Flusher).Flush()
+		case <-r.Context().Done():
+			return
+		}
+	}
+}
--- server/main.go
+++ server/main.go
@@ -8,6 +8,7 @@ import (
 func main() {
 	//  handler for events streaming...
 	http.HandleFunc("/events", handleEventsStream)
+	http.HandleFunc("/newevents", eventsHandler)
 	// Start the server
 	fmt.Println("SSE server listening on :8080")
 	http.ListenAndServe(":8080", nil)
